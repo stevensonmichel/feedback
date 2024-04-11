41e59e48264c281abff78714e1670f572faa5711
Stevenson <michels@berea.edu>
2024-04-10 21:49:39 -0400

    Added POST request function and file

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 def review(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        print(username)
+        return HttpResponseRedirect("/thank-you")
+    
     return render(request, "reviews/review.html")
-    pass
+    
+def thank_you(request):
+    return render(request, "reviews/thank_you.html") 
+    
