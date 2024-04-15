18872569836b68de8b574851608eb4834a4dce7a
Stevenson <michels@berea.edu>
2024-04-15 07:47:55 -0400

    Added session variable information to the front end

--- feedback/settings.py
+++ feedback/settings.py
@@ -125,6 +125,8 @@ MEDIA_ROOT = BASE_DIR / "uploads"
 
 MEDIA_URL = "/user-media/"
 
+SESSION_COOKIE_AGE = 120
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- reviews/views.py
+++ reviews/views.py
@@ -75,6 +75,14 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
     
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.get_object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+    
     
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -84,3 +92,9 @@ class SingleReviewView(DetailView):
     #     return context
 
     
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("reviews/" + review_id)
